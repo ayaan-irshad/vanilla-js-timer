30a69f0ed7bba71b79cbfd84d9e31e95cfee48cb
ayaan-irshad <ayaanirshadbhat@gmail.com>
2021-03-08 13:47:30 +0530

    vanilla javascript countdown timer

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function countdownTimer() {
+  const difference = +new Date("2022-01-01") - +new Date();
+  let remaining = "Time's up";
+
+  if (difference > 0) {
+    const parts = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / (1000)) % 60)
+    };
+
+    remaining = Object.keys(parts)
+      .map((part) => {
+        return `<div> ${parts[part]} <span> ${part} </span> </div>`;
+      })
+      .join(" ");
+
+  }
+
+
+  document.getElementById("countdown").innerHTML = remaining;
+}
+
+countdownTimer();
+setInterval(countdownTimer, 1000);
